372f0a2ba8dfbf1e7b259b2ce066f873b009f387
Maksim Koltsov <tirinox@gmail.com>
2020-12-08 09:22:39 +0300

    no more multi node list load

--- backend/src/midgard/value_filler.py
+++ backend/src/midgard/value_filler.py
@@ -19,20 +19,24 @@ class ValueFiller:
         self.pool_price_cache = PoolPriceCache()
         self.total_number = None
         self.processed_number = 0
+        self.fetcher = PoolPriceFetcher()
 
     async def run(self, interval=20, blocking=True):
         _, self.total_number = await BEPTransaction.random_tx_without_volume()
 
-        tasks = []
-        for i in range(self.n_workers):
-            if interval > 0:
-                t = schedule_task_periodically(interval, self._worker)
-            else:
-                t = asyncio.create_task(self._worker())
-            tasks.append(t)
+        async with aiohttp.ClientSession() as session:
+            self.fetcher.session = session
+            await self.fetcher.load_nodes_ip()
+            tasks = []
+            for i in range(self.n_workers):
+                if interval > 0:
+                    t = schedule_task_periodically(interval, self._worker)
+                else:
+                    t = asyncio.create_task(self._worker())
+                tasks.append(t)
 
-        if blocking:
-            await asyncio.gather(*tasks)
+            if blocking:
+                await asyncio.gather(*tasks)
 
     async def _give_up_tx(self, tx: BEPTransaction, logger):
         logger.warning(f'give up on tx: {tx}')
@@ -42,40 +46,43 @@ class ValueFiller:
         await tx.save()
 
     def _blacklist_tx(self, tx: BEPTransaction):
-        n = self.tx_black_list[tx.hash] = self.tx_black_list.get(tx.hash, 0) + 1
-        return n
+        if tx is not None:
+            n = self.tx_black_list[tx.hash] = self.tx_black_list.get(tx.hash, 0) + 1
+            return n
 
     def _times_blacklisted(self, tx: BEPTransaction):
-        return self.tx_black_list.get(tx.hash, 0)
+        if tx is not None:
+            return self.tx_black_list.get(tx.hash, 0)
+        else:
+            return 0
 
     async def _worker(self):
         name = f"Value Filler Worker {names.get_full_name()}"
         logger = logging.getLogger(name)
 
-        async with aiohttp.ClientSession() as session:
-            fetcher = PoolPriceFetcher(session)
-            while True:
-                try:
-                    tx, n = await BEPTransaction.random_tx_without_volume()
-                    if not tx or n == 0:
-                        logger.info('no more TX to fill, worker quits.')
-                        break
+        tx = None
+        while True:
+            try:
+                tx, n = await BEPTransaction.random_tx_without_volume()
+                if not tx or n == 0:
+                    logger.info('no more TX to fill, worker quits.')
+                    break
 
-                    if tx.date <= self.DONT_FILL_RUNE_PRICE_BEFORE or self._times_blacklisted(tx) >= 3:
-                        await self._give_up_tx(tx, logger)
-                        continue
+                if tx.date <= self.DONT_FILL_RUNE_PRICE_BEFORE or self._times_blacklisted(tx) >= 3:
+                    await self._give_up_tx(tx, logger)
+                    continue
 
-                    await tx.fill_tx_volume_and_usd_prices(self.pool_price_cache, fetcher)
-                    await tx.save()
+                await tx.fill_tx_volume_and_usd_prices(self.pool_price_cache, self.fetcher)
+                await tx.save()
 
-                    self.processed_number += 1
-                    logger.info(
-                        f'filled usd data for #{tx.id} ({tx.rune_volume:.1f} R). '
-                        f'{self.processed_number}/{self.total_number}, '
-                        f'{progressbar(self.processed_number, self.total_number, 20)}'
-                    )
-                except Exception:
-                    logger.exception(f'error, I will sleep for a little while {tx.hash} ({tx})',
-                                     exc_info=False)
-                    self._blacklist_tx(tx)
-                    await asyncio.sleep(3.0)
+                self.processed_number += 1
+                logger.info(
+                    f'filled usd data for #{tx.id} ({tx.rune_volume:.1f} R). '
+                    f'{self.processed_number}/{self.total_number}, '
+                    f'{progressbar(self.processed_number, self.total_number, 20)}'
+                )
+            except Exception:
+                logger.exception(f'error, I will sleep for a little while {tx.hash if tx else ""} ({tx})',
+                                 exc_info=False)
+                self._blacklist_tx(tx)
+                await asyncio.sleep(3.0)
